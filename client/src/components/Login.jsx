769c0d8e119ee362caa57cbc681561f93316973b
hrsh1n1 <harshinidadhich8@gmail.com>
2025-12-07 01:25:14 +0530

    frontend files moved to 'client' directory

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 import { AppContext } from '../context/AppContext';
+import { motion } from "motion/react"
+
 
 const Login = () => {
 
@@ -15,10 +17,15 @@ const Login = () => {
     }, [])
 
   return (
-    <div className='absolute top-0 left-0 right-0 bottom-0
+    <div className='fixed top-0 left-0 right-0 bottom-0
      z-10 backdrop-blur-sm bg-black/30 flex justify-center
      items-center'>
-        <form className='relative bg-white p-10 rounded-xl 
+        <motion.form 
+        initial={{opacity: 0.2, y: 100}}
+        transition={{duration: 1}}
+        whileInView={{opacity : 1, y: 0}}
+        viewport={{once: true}}
+        className='relative bg-white p-10 rounded-xl 
         text-slate-500'>
             <h1 className='text-center text-2xl text-neutral-700
             font-medium'>{state}</h1>
@@ -53,7 +60,7 @@ const Login = () => {
 
             <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} className='absolute top-5 right-5 cursor-pointer'/>
 
-        </form>
+        </motion.form>
 
     </div>
   )
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import { motion } from "motion/react"
 
 const Result = () => {
 
@@ -16,7 +17,12 @@ const Result = () => {
 
     
 
-    <form onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <motion.form 
+    initial={{opacity: 0.2, y: 100}}
+    transition={{duration: 1}}
+    whileInView={{opacity : 1, y: 0}}
+    viewport={{once: true}}
+    onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'>
     <div>
       <div className='relative'>
         <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
@@ -54,7 +60,7 @@ const Result = () => {
     </div>
   }
 
-    </form>
+    </motion.form>
   )
 }
 
